49b657464e01cc688b45fb323dc664de61a8b4c3
SilviaGuerra <silvia.guerrahdz@gmail.com>
2023-03-21 15:07:55 -0600

    Fix route api

--- src/store/slices/busqueda/index.js
+++ src/store/slices/busqueda/index.js
@@ -24,7 +24,7 @@ export default meliSlice.reducer;
 export const fetchAllproducts = (results) => (dispatch) => {
   axios
     .get(
-      `https://seashell-app-fpkyl.ondigitalocean.app//api/items?q=${results}&limit=4`
+      `https://seashell-app-fpkyl.ondigitalocean.app/api/items?q=${results}&limit=4`
     )
     .then((response) => {
       dispatch(resultado(response.data));
@@ -33,7 +33,7 @@ export const fetchAllproducts = (results) => (dispatch) => {
 };
 export const fetchItemDetails = (id) => (dispatch) => {
   axios
-    .get(`https://seashell-app-fpkyl.ondigitalocean.app//api/items/${id}`)
+    .get(`https://seashell-app-fpkyl.ondigitalocean.app/api/items/${id}`)
     .then((response) => {
       dispatch(details(response.data));
     })
