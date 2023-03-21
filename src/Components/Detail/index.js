e2aa058798dd7ac52b95afc239f688517af5016e
SilviaGuerra <silvia.guerrahdz@gmail.com>
2023-03-21 02:58:08 -0600

    Changed structure and add classes and styles to details

--- src/Components/Detail/index.js
+++ src/Components/Detail/index.js
@@ -3,6 +3,7 @@ import { Container, Breadcrumb, Row, Col, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchItemDetails } from "../../store/slices/busqueda";
 import { useParams } from "react-router-dom";
+import "./styles.scss";
 
 const Detail = () => {
   const details = useSelector((state) => state.mercadoL.details);
@@ -13,18 +14,20 @@ const Detail = () => {
     dispatch(fetchItemDetails(params.id));
   }, [dispatch, params.id]);
 
-  // console.log(details.price, details.price.amount);
   return (
     <Container>
       <Breadcrumb>
-        <Breadcrumb.Item active>Home</Breadcrumb.Item>
-        <Breadcrumb.Item active>Library</Breadcrumb.Item>
-        <Breadcrumb.Item active>Data</Breadcrumb.Item>
+        <Breadcrumb.Item href="/" className="breadcrumb-container">
+          Inicio
+        </Breadcrumb.Item>
+        <Breadcrumb.Item href="/" className="breadcrumb-container">
+          Atrás
+        </Breadcrumb.Item>
       </Breadcrumb>
       <section className="body-container">
         <>
           <Row>
-            <Col lg="8">
+            <Col xs={12} md={8}>
               <section className="detail-img">
                 <img
                   src={details.picture}
@@ -33,13 +36,13 @@ const Detail = () => {
                 />
               </section>
             </Col>
-            <Col lg="3">
+            <Col xs={12} md={4}>
               <section className="detail-information">
-                <span>
+                <span className="detail-conditions">
                   {details.condition} - {details.sold_quantity} vendidos
                 </span>
-                <p>{details.title}</p>
-                <p>
+                <p className="detail-title">{details.title}</p>
+                <p className="detail-price">
                   ${" "}
                   {details &&
                   details.price &&
@@ -47,13 +50,13 @@ const Detail = () => {
                     ? details.price.amount
                     : 0}
                 </p>
-                <Button>Comprar</Button>
+                <Button className="detail-buy">Comprar</Button>
               </section>
             </Col>
           </Row>
-          <Row>
-            <p>Descripción del producto</p>
-            <p>{details.description}</p>
+          <Row className="detail-description">
+            <p className="detail-description_title">Descripción del producto</p>
+            <p className="detail-description_text">{details.description}</p>
           </Row>
         </>
       </section>
